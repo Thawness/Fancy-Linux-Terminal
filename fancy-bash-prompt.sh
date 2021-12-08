c2695148fc19be34784a66b3d68ad78958d51d68
JoDarhk <48790098+JoDarhk@users.noreply.github.com>
2021-12-08 19:13:02 +0530

    Update fancy-bash-prompt.sh

--- fancy-bash-prompt.sh
+++ fancy-bash-prompt.sh
@@ -4,7 +4,7 @@
 ##	|                                                                       |
 ##	|                            FANCY BASH PROMT                           |
 ##	|                                                                       |
-##	| Copyright (c) 2018, Andres Gongora <mail@andresgongora.com>.          |
+##	| Copyright (c) 2018      |
 ##	|                                                                       |
 ##	| This program is free software: you can redistribute it and/or modify  |
 ##	| it under the terms of the GNU General Public License as published by  |
